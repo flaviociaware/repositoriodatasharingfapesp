9cdde8e3b765fbd216b874b2494b141e7c376990
Flavio Barbosa <flavio@ciaware.com.br>
2023-01-30 10:32:27 -0300

    Adicionando arquivos do Hospital Sirio Libanes

---
+++ arquivos-fonte/HSL_Junho2021-2020-06-30/CRIAR-TABELA-PARA-IMPORTAR-ARQUIVO-HSL_Pacientes_4.sql
@@ -0,0 +1,14 @@
+CREATE TABLE HSL_Pacientes_4 (
+  ID_PACIENTE VARCHAR(60) NOT NULL PRIMARY KEY
+, IC_SEXO VARCHAR(1) 
+, AA_NASCIMENTO VARCHAR(4)
+, CD_PAIS VARCHAR(2)
+, CD_UF VARCHAR(2)
+, CD_MUNICIPIO VARCHAR(255)
+, CD_CEPREDUZIDO VARCHAR(8)
+);
+
+
+\copy HSL_Pacientes_4 
+FROM '/Users/fbarbosa/git/repositoriodatasharingfapesp/arquivos-fonte/HSL_Junho2021-2020-06-30/HSL_Pacientes_4.csv' 
+csv header delimiter '|'
